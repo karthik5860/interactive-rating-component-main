074425360e494a2f77c696bae4422c8364883295
karthik5860 <65306091+karthik5860@users.noreply.github.com>
2022-09-20 22:45:39 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,12 @@
+  var ratingcard = document.getElementById("ratingcard");
+  function switcH() {
+    ratingcard.style.display="none"
+    endingcard.style.display="initial"
+  }
+
+  document.ratingcircle.onclick = function(){
+    var rating = document.ratingcircle.rating.value;
+    result.innerHTML = rating
+  }
+    result.innerHTML = "nothing"
+
